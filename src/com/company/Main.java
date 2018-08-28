9bb03720f5e8321c33aa028f2670034d9b9442e3
Tanner J Wildasin <tw073@pvhs-112-d23.pvsd.org>
2018-08-28 11:07:18 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
